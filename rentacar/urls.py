6f85dbb4399c6f7b412915e894d7f6f71e50e596
Ertan <kadimertan78@gmail.com>
2023-09-22 20:01:24 +0300

    site dinamik hale getirildi

---
+++ rentacar/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('cars/', views.cars, name='cars'),
+    path('car/<slug>/', views.car_detail, name='car_detail'),
+]
--- rentacar/views.py
+++ rentacar/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'pages/index.html')
+
+def cars(request):
+    return render(request, 'pages/cars.html')
+
+def car_detail(request, slug):
+    return render(request, 'pages/car_detail.html')

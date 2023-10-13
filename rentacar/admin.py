65bad975b3c89ac64c361c74a3a824044aee9ef7
Ertan <kadimertan78@gmail.com>
2023-10-14 00:34:22 +0300

    models duzeltildi. hatalar giderildi

--- rentacar/admin.py
+++ rentacar/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
-from .models import Car, Category, Booking
+from .models import Car, Booking, Brand, Series, Model, FuelType, TransmissionType, CaseType, VehicleType
 
 class CarAdmin(admin.ModelAdmin):
-    list_display = ['category', 'brand', 'model', 'year', 'is_available', 'is_damaged', 'price_per_day']
-    list_filter = ['category']
+    list_display = ['brand', 'series', 'model', 'year', 'is_available', 'is_damaged', 'plate_number']
     list_display_links = ['brand', 'model', 'year']
 
     class Meta:
         model = Car
 
 admin.site.register(Car, CarAdmin)
-admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Series)
+admin.site.register(Model)
+admin.site.register(FuelType)
+admin.site.register(TransmissionType)
+admin.site.register(VehicleType)
+admin.site.register(CaseType)
 admin.site.register(Booking)
--- rentacar/models.py
+++ rentacar/models.py
@@ -1,47 +1,124 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 from autoslug import AutoSlugField
 
-class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Kategori Adı")
-    slug = AutoSlugField(populate_from='name', unique=True, verbose_name="Slug")
+def validate_year(value):
+    current_year = timezone.now().year
+    if value > current_year:
+        raise ValidationError(f"{value} yılı geçerli değil. Yıl en fazla {current_year} olabilir.")
 
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Marka Adı")
+    
     def __str__(self):
         return self.name
     
     class Meta:
-        verbose_name = "Kategori"
-        verbose_name_plural = "Kategoriler"
+        verbose_name = "Marka"
+        verbose_name_plural = "Markalar"
         ordering = ['name']
 
-class Car(models.Model):
-    # Temel Bilgiler
-    brand = models.CharField(max_length=100, verbose_name="Marka")
-    model = models.CharField(max_length=100, verbose_name="Model")
-    year = models.IntegerField(verbose_name="Yıl")
-    slug = AutoSlugField(populate_from='get_slug', unique=True, verbose_name="Slug")
+class Series(models.Model):
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='series')
+    name = models.CharField(max_length=100, verbose_name="Seri Adı")
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Seri"
+        verbose_name_plural = "Seriler"
+        ordering = ['name']
 
+# Adjust the Model class to have a ForeignKey to Series
+class Model(models.Model):
+    series = models.ForeignKey(Series, on_delete=models.CASCADE, null=True, related_name='models')  # Add this line
+    name = models.CharField(max_length=100, verbose_name="Model Adı")
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Model"
+        verbose_name_plural = "Modeller"
+        ordering = ['name']
+
+
+class FuelType(models.Model):
+    type = models.CharField(max_length=50, verbose_name="Yakıt Türü")
+    
+    def __str__(self):
+        return self.type
+    
+    class Meta:
+        verbose_name = "Yakıt Türü"
+        verbose_name_plural = "Yakıt Türleri"
+        ordering = ['type']
+
+class TransmissionType(models.Model):
+    type = models.CharField(max_length=50, verbose_name="Vites Türü")
+    
+    def __str__(self):
+        return self.type
+    
+    class Meta:
+        verbose_name = "Vites Türü"
+        verbose_name_plural = "Vites Türleri"
+        ordering = ['type']
+
+class VehicleType(models.Model):
+    type = models.CharField(max_length=50, verbose_name="Araç Tipi")
+    
+    def __str__(self):
+        return self.type
+    
+    class Meta:
+        verbose_name = "Araç Tipi"
+        verbose_name_plural = "Araç Tipleri"
+        ordering = ['type']
+
+class CaseType(models.Model):
+    type = models.CharField(max_length=50, verbose_name="Kasa Tipi")
+    
+    def __str__(self):
+        return self.type
+    
+    class Meta:
+        verbose_name = "Kasa Tipi"
+        verbose_name_plural = "Kasa Tipleri"
+        ordering = ['type']
+
+class Car(models.Model):
     # Kategori ve Durum Bilgileri
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Kategori")
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, verbose_name="Marka")
+    series = models.ForeignKey(Series, on_delete=models.CASCADE, verbose_name="Seri")
+    model = models.ForeignKey(Model, on_delete=models.CASCADE, verbose_name="Model")
+    year = models.IntegerField(validators=[validate_year], verbose_name="Yıl")
     is_available = models.BooleanField(default=False, verbose_name="Kiralık Mı?")
     is_damaged = models.BooleanField(default=False, verbose_name="Hasarlı Mı?")
 
     # Teknik Özellikler
+    vehicle_type = models.ForeignKey(VehicleType, on_delete=models.SET_NULL, null=True, verbose_name="Araç Tipi")
+    case_type = models.ForeignKey(CaseType, on_delete=models.SET_NULL, null=True, verbose_name="Kasa Tipi")
     km = models.IntegerField(verbose_name="Kilometre")
+    fuel_type = models.ForeignKey(FuelType, on_delete=models.SET_NULL, null=True, verbose_name="Yakıt Türü")
+    transmission_type = models.ForeignKey(TransmissionType, on_delete=models.SET_NULL, null=True, verbose_name="Vites Türü")
     number_of_doors = models.CharField(max_length=2, verbose_name="Kapı Sayısı")
+    number_of_seats = models.CharField(max_length=2, null=True, verbose_name="Koltuk Sayısı")
     luggage_volume = models.CharField(max_length=50, verbose_name="Bagaj Hacmi")
-    fuel_type = models.CharField(max_length=50, verbose_name="Yakıt Türü")
-    transmission_type = models.CharField(max_length=50, verbose_name="Vites Türü")
     color = models.CharField(max_length=50, verbose_name="Renk")
-    number_of_person = models.CharField(max_length=2, verbose_name="Kişi Sayısı")
+    # number_of_person = models.CharField(max_length=2, verbose_name="Kişi Sayısı")
 
     # Kiralama Bilgileri
     price_per_day = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Günlük Fiyat")
-    pickup_location = models.CharField(null=True, blank=True, max_length=255, verbose_name="Alış Yeri")
-    dropoff_location = models.CharField(null=True, blank=True, max_length=255, verbose_name="Teslim Yeri")
-    start_date = models.DateField(null=True, blank=True, verbose_name="Kiralanma Tarihi")
-    start_time = models.TimeField(null=True, blank=True, verbose_name="Kiralanma Saati")
-    return_date = models.DateField(null=True, blank=True, verbose_name="Teslim Tarihi")
-    return_time = models.TimeField(null=True, blank=True, verbose_name="Teslim Saati")
+    # pickup_location = models.CharField(null=True, blank=True, max_length=255, verbose_name="Alış Yeri")
+    # dropoff_location = models.CharField(null=True, blank=True, max_length=255, verbose_name="Teslim Yeri")
+    # start_date = models.DateField(null=True, blank=True, verbose_name="Kiralanma Tarihi")
+    # start_time = models.TimeField(null=True, blank=True, verbose_name="Kiralanma Saati")
+    # return_date = models.DateField(null=True, blank=True, verbose_name="Teslim Tarihi")
+    # return_time = models.TimeField(null=True, blank=True, verbose_name="Teslim Saati")
 
     # Görseller
     image1 = models.URLField(null=True, blank=True, verbose_name="1. Fotoğrak Linki")
@@ -49,15 +126,15 @@ class Car(models.Model):
     image3 = models.URLField(null=True, blank=True, verbose_name="3. Fotoğrak Linki")
     image4 = models.URLField(null=True, blank=True, verbose_name="4. Fotoğrak Linki")
 
-    def __str__(self):
-        return f"{self.brand} {self.model} ({self.year})"
-    
+    plate_number = models.CharField(max_length=10, unique=True, verbose_name="Plaka Numarası", null=True, blank=True)
+    slug = AutoSlugField(populate_from='get_slug', unique=True, verbose_name="Slug")
+
     def get_slug(self):
-        slug = f"{self.brand}-{self.model}-{self.year}"
-        car = Car.objects.filter(slug=slug).first()
-        if car:
-            slug = f"{slug}-{self.id}"
+        slug = f"{self.brand.name}-{self.series.name}-{self.model.name}-{self.year}-{self.id}"  # Adjusted to include series
         return slug
+
+    def __str__(self):
+        return f"{self.brand} {self.series} {self.model} ({self.year})"  # Adjusted to include series
     
     class Meta:
         verbose_name = "Araba"
@@ -90,3 +167,4 @@ class Booking(models.Model):
         verbose_name = "Rezervasyon"
         verbose_name_plural = "Rezervasyonlar"
         ordering = ['-id']
+
